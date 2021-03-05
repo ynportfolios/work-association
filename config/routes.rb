07e0c7289791faebb190d187c4c099a3d065a2a2
ynportfolios <y.n.portfolios@gmail.com>
2021-03-05 17:21:22 +0900

    ルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :blogs
   resources :users, only: [:new, :create, :show]
