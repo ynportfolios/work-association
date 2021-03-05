becca3a83b67c4f6bc38a714f075b5ac4262d56b
ynportfolios <y.n.portfolios@gmail.com>
2021-03-05 17:41:19 +0900

    アソシエーションの作成

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,6 @@
 class Blog < ApplicationRecord
+
+  belongs_to :user
+
+  has_many :favorites, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :blogs
   resources :users, only: [:new, :create, :show]
+  resources :favorites, only: [:create, :destroy, :show]
 end
